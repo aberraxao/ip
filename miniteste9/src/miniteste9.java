5fc174279112e7ba5e5763cf4b5f8d7f1fcb7e89
aberraxao <diana_milu@live.com.pt>
2021-12-16 18:32:21 +0000

    mini test 9

---
+++ miniteste9/src/miniteste9.java
@@ -0,0 +1,43 @@
+
+
+public class miniteste9 {
+
+	 public static void main(String[] a) {
+		// System.out.print("Diana");
+		// System.out.print("END");
+		 
+	        for(int i = 33; i != 128; i++)
+	            System.out.print(i + ": " + (char)i + "  ");
+	       System.out.println("END");
+	    }
+
+	}
+
+/*
+public class miniteste92 {
+
+    public static void main(String[] a) {
+
+        for(int i = 0; i != 6; i++) {
+
+            System.out.print('*');
+
+        }
+
+    }
+
+}
+
+public class miniteste91 {
+
+    public static void main(String[] a) {
+
+       for(int i = 0; i != 6; i++) {
+
+           System.out.println('*');
+
+       }
+
+    }
+
+}*/
